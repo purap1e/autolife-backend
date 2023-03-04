85a04db5c51078619dcd08650592ef5c81e62f8d
Duman Sabyrbayev <90duman@mail.ru>
2023-03-04 20:49:48 +0600

    refactor

--- tax-service/src/main/java/kz/auto_life/taxservice/configs/SecurityConfig.java
+++ tax-service/src/main/java/kz/auto_life/taxservice/configs/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
