e204a295322e3d6444a0475e2b88ff108abd15ba
Daniyar Bulegenov <purap1e@mail.ru>
2023-03-25 19:08:40 +0600

    feature/BT5-23: shop service added

---
+++ shop-service/src/main/java/kz/auto_life/shopservice/ShopServiceApplication.java
@@ -0,0 +1,18 @@
+package kz.auto_life.shopservice;
+
+import kz.auto_life.shopservice.properties.JwtProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties({JwtProperties.class})
+public class ShopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopServiceApplication.class, args);
+    }
+
+}
---
+++ shop-service/src/main/java/kz/auto_life/shopservice/properties/JwtProperties.java
@@ -0,0 +1,13 @@
+package kz.auto_life.shopservice.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("jwt")
+@Getter
+@Setter
+public class JwtProperties {
+    private String secret;
+    private int expiresAt;
+}
