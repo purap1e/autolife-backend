9c586a152a020d577ff839b8bbad496193e07225
Daniyar Bulegenov <purap1e@mail.ru>
2023-03-26 14:07:52 +0600

    feature/BT5-23: swagger fixed in auth

--- auth-service/src/main/java/kz/auto_life/authservice/controllers/UserController.java
+++ auth-service/src/main/java/kz/auto_life/authservice/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Auth API")
+@Tag(name = "User API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user/")
